8e8afee395fb06c442cf78adb7f88d1faf650235
OlgaKel <nikkelos@ya.ru>
2023-11-30 15:31:48 +0300

    addede adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,67 @@
 $(function(){
+ 
+  /**$(".rate-star").rateYo({
+    rating: 5,
+    starWidth:"12px",
+    readOnly: true
+  });**/
 
  
   $('.products__slider-inner').slick({
-dots:true,
-arrows:false,
-slidesToShow: 4,
-slidesToScroll:4
+   dots:true,
+   arrows:false,
+   slidesToShow: 4,
+   slidesToScroll:4,
+   responsive: [
+    {
+      breakpoint: 1900,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 1441,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 801,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    ]
   });
 
-  $('.menu__btn').on('click', function(){
-    $('.menu__list').slideToggle();
-  });
- 
-  $('.header__btn-menu').on('click', function(){
-    $('.header__box').toggleClass('active');
-  });
+  /**$(".js-range-slider")/ionRangeSlider({
+    type:"double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to:600,
+    prefix: "$"
+  }):**/
 
   Fancybox.bind("[data-fancybox]", {
     // Your custom 
   });
+  
+/**$('.icon-th-list').on('click', function()){
+ *  $('.product__item').addClass('list');
+ *  $('.icon-th-list').addClass('active');
+ *  $('.icon-th-large').removeClass('active');
+  })*/
+
+  /**$('.icon-th-large').on('click', function()){
+ *  $('.product__item').removeClass('list');
+ *  $('.icon-th-large').addClass('active');
+ *  $('.icon-th-list').removeClass('active');
+  })*/
+
 
   $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
     var id = $(this).attr('data-id');
@@ -31,11 +74,14 @@ slidesToScroll:4
     
     $('input[type="file"], select').styler();
 
- /**$(".rate-star").rateYo({
-    rating: 5,
-    starWidth:"12px",
-    readOnly: true
-  });**/
+    $('.menu__btn').on('click', function(){
+      $('.menu__list').slideToggle();
+    });
+   
+    $('.header__btn-menu').on('click', function(){
+      $('.header__box').toggleClass('active');
+    });
+  
 var mixer = mixitup('.products__inner-box');
 
 }); 
